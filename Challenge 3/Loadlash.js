1c0697bbe19064e107ad8886711f8cfef5e01e7a
RahulChandranna <91324312+RahulChandranna@users.noreply.github.com>
2021-09-24 18:01:26 +0530

    Update Loadlash.js

--- Challenge 3/Loadlash.js
+++ Challenge 3/Loadlash.js
@@ -1,3 +1,16 @@
+# +------------------------------------------------------------------------------------
+# | File : 	Get_NestedObject.JS
+# | Version : 	1.00
+# | Author:	Rahul C
+# | Purpose : Function that you pass in the object and a key and get back the value.
+# +------------------------------------------------------------------------------------
+# | Maintenance History
+# | -------------------
+# | Name               Date       Version   C/R      Description
+# | -----------------------------------------------------------------------------------
+# | Rahul C     23-09-2021   1.00    Creation
+# +------------------------------------------------------------------------------------
+
 
 // Load the full build
 const _ = require('lodash'); 
@@ -9,14 +22,14 @@ var object = {"a": { "b": { "c": "d" }}};
 console.log("Result at 'a/b/c': ",_.get(object, 'a.b.c'));
 
 //Defining Object
-var objec1 = {"x":{"y":{"z":"a"}}};
+var object1 = {"x":{"y":{"z":"a"}}};
 
 //Loadlash _.get() function is used to fetch the values here based on the inputs
 console.log("Result at 'x/y/z': ",_.get(object1, 'x.y.z'));
 
 
 
-Have this in head section of your website
-<script scr="https://cdn.jsdelivr.net/npm/lodash@4.17.10/lodash.min.js"></script>
+//Have this in head section of your website
+//<script scr="https://cdn.jsdelivr.net/npm/lodash@4.17.10/lodash.min.js"></script>
 
-Put the above functions there , it will work
+
